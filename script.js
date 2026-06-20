3ea8278b69dfa88c6fb7184ec00aa80bb2232f95
Jade-Souza <jadegiulia3817@gmail.com>
2026-06-20 20:37:57 -0300

    Corrigindo tela inicial

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ document
     window.location.href = "calendar.html";
 
 });
+
+const stars =
+document.querySelector(".stars");
+
+for(let i = 0; i < 50; i++){
+
+    const star =
+    document.createElement("div");
+
+    star.classList.add("star");
+
+    star.style.left =
+    Math.random() * 100 + "%";
+
+    star.style.top =
+    Math.random() * 100 + "%";
+
+    star.style.animationDelay =
+    Math.random() * 3 + "s";
+
+    stars.appendChild(star);
+}
